d94a99fdbdab5e644c3ec235e217452d1ade56d8
himesh220002 <satyamhimesh@gmail.com>
2025-05-23 15:58:39 +0530

    re

--- src/listing-details/components/Features.tsx
+++ src/listing-details/components/Features.tsx
@@ -1,39 +1,45 @@
-import { FaCheck } from "react-icons/fa";
-import { ImCross } from "react-icons/im";
 
-type FeatureProps = {
-  features: Record<string, boolean>;
-};
+import { FaCheck} from "react-icons/fa";
+import { ImCross  } from "react-icons/im";
 
-function Features({ features }: FeatureProps) {
-  if (!features) return null;
+type featu ={
+    features: any
+}
 
-  return (
-    <div className='p-4 md:p-10 mt-7 bg-white rounded-xl border shadow-md'>
-      <h2 className='text-xl md:text-2xl font-semibold'>Features</h2>
 
-      <div
-        className={`
+function Features({ features }: featu) {
+    if (!features) return null; 
+
+   
+    return (
+        
+            <div className='p-4 md:p-10 mt-7  bg-white rounded-xl border shadow-md '>
+                <h2 className='text-xl md:text-2xl font-semibold'>Features</h2>
+                <div 
+                className={`
           grid gap-4 mt-4 
           grid-cols-1 
           sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 
           sm:max-h-none 
-          max-h-[400px] overflow-y-auto
+          max-h-[800px] overflow-y-auto
         `}
-      >
-        {Object.entries(features).map(([key, value]) => (
-          <div key={key} className='flex items-center gap-2'>
-            {value ? (
-              <FaCheck className='text-sm md:text-lg p-1 rounded-full bg-blue-100 text-blue-600' />
-            ) : (
-              <ImCross className='text-sm md:text-lg p-1 rounded-full bg-red-100 text-red-500' />
-            )}
-            <h2 className='text-sm break-words'>{key}</h2>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+                >
+                    {Object.entries(features).map(([features,value])=>(
+                        <div key={features} className='flex  items-center gap-2'>
+                            {value ? (<FaCheck className='text-sm md:text-lg p-1 rounded-full bg-blue-100 text-blue-600'/>):(<ImCross  className='text-sm md:text-lg p-1 rounded-full bg-red-100 text-red-500'/>)}
+                            {/* <FaCheck className='text-lg p-1 rounded-full text-green-600'/> */}
+                           <h2 className='text-sm break-all'> {features}</h2>
+                        </div>
+                    ))}
+                </div>
+                {/* {[features]?.map((item,index)=>(
+                    <div key={index}>
+                        <FaCheck className='text-lg bg-blue-100 text-blue-700'/>
+                    </div>
+                ))} */}
+            </div>
+        
+    )
 }
 
-export default Features;
+export default Features
