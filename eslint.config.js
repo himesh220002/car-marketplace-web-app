c1fd523102f6feaec53ab9210dc7bfd447e3c2f6
Himesh Satyam <satyamhimesh@gmail.com>
2025-12-01 13:52:16 +0530

    m

--- eslint.config.js
+++ eslint.config.js
@@ -7,6 +7,7 @@ import { defineConfig } from "eslint/config";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
+  { ignores: ["dist/**", "node_modules/**"] },
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
